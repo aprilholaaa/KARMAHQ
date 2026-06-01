8e0fe5cd3457f0dad29f61aef127f4e743f6cd71
saket <aprilholaaa@gmail.com>
2026-06-01 14:24:15 +0530

    Improved Reddit request headers

--- index.js
+++ index.js
@@ -548,9 +548,16 @@ const response = await axios.get(
   `https://www.reddit.com/user/${username}/about.json?raw_json=1`,
   {
     headers: {
-      'User-Agent':
-        'discord:karmahq:v1.0 (by /u/spez)'
-    },
+      headers: {
+  'User-Agent':
+    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/137.0.0.0 Safari/537.36',
+
+  'Accept':
+    'application/json',
+
+  'Accept-Language':
+    'en-US,en;q=0.9'
+},    },
 
     httpsAgent: proxyAgent,
 
